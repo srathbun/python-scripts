e637772086bb50032579cfe08978a6efba8422ee
Spencer Rathbun <spencerrathbun@gmail.com>
2012-01-06 15:53:04 -0500

    Added links

--- samples.py
+++ samples.py
@@ -4,7 +4,10 @@
 # Python Lex-Yacc
 # http://www.dabeaz.com/ply/
 
+# python 25 scripts: http://pythoncodingforum.com/showthread.php?tid=3&pid=3#pid3
 
+# console hacks: http://matt.might.net/articles/console-hacks-exploiting-frequency/
+# roll your own unix: http://www.jamesmolloy.co.uk/tutorial_html/
 def combineDicts(dictionary1, dictionary2):
 	output = {}
 	for item, value in dictionary1.iteritems():
@@ -15,5 +18,4 @@ def combineDicts(dictionary1, dictionary2):
 			output[item] = value
 	for item, value in dictionary2.iteritems():
 		output[item] = value
-
 	return output
